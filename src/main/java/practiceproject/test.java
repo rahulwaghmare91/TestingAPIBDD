7a255ba4c2c2676035e8473361e988f9b173a1db
rahulwaghmare92 <rahulwa597@gmail.com>
2022-08-06 22:26:23 -0700

    add1 hi

--- src/main/java/practiceproject/test.java
+++ src/main/java/practiceproject/test.java
@@ -7,6 +7,8 @@ public class test {
 public static void main (String[] args) {
 	 System.setProperty("webdriver.chrome.driver","E:\\cucumber practice project\\Cucumber-for-paractice\\Drivers\\chromedriver.exe");
    WebDriver   driver=new ChromeDriver();	
+   
+   System.out.println("hi");
 
 }
 }
